abcacb6a6643df0e73e32418e251006cabdb0f00
mearvk <mearvk@outlook.com>
2026-08-08 22:19:16 -0400

    Science Commit 5.

---
+++ userland/x11/_build/libbsd-0.12.2/config.h
@@ -0,0 +1,458 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the 'asprintf' function. */
+#define HAVE_ASPRINTF 1
+
+/* Define to 1 if you have the 'clearenv' function. */
+#define HAVE_CLEARENV 1
+
+/* Define to 1 if you have the declaration of 'environ', and to 0 if you
+   don't. */
+#define HAVE_DECL_ENVIRON 1
+
+/* Define to 1 if you have the <dirent.h> header file. */
+#define HAVE_DIRENT_H 1
+
+/* Define to 1 if you have the 'dirfd' function. */
+#define HAVE_DIRFD 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have fcntl(F_CLOSEM) */
+/* #undef HAVE_FCNTL_CLOSEM */
+
+/* Define to 1 if you have the 'flock' function. */
+#define HAVE_FLOCK 1
+
+/* Define to 1 if you have the 'fopencookie' function. */
+#define HAVE_FOPENCOOKIE 1
+
+/* Define to 1 if you have the 'funopen' function. */
+/* #undef HAVE_FUNOPEN */
+
+/* Define to 1 if you have the 'getauxval' function. */
+#define HAVE_GETAUXVAL 1
+
+/* Define to 1 if you have the 'getentropy' function. */
+#define HAVE_GETENTROPY 1
+
+/* Define to 1 if you have the 'getexecname' function. */
+/* #undef HAVE_GETEXECNAME */
+
+/* Define to 1 if you have the 'getline' function. */
+#define HAVE_GETLINE 1
+
+/* Define to 1 if you have the 'gid_from_group' function. */
+/* #undef HAVE_GID_FROM_GROUP */
+
+/* Define to 1 if you have the 'group_from_gid' function. */
+/* #undef HAVE_GROUP_FROM_GID */
+
+/* Define to 1 if you have the <grp.h> header file. */
+#define HAVE_GRP_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <minix/config.h> header file. */
+/* #undef HAVE_MINIX_CONFIG_H */
+
+/* Define to 1 if you have the <ndir.h> header file. */
+/* #undef HAVE_NDIR_H */
+
+/* Define to 1 if you have the 'open_memstream' function. */
+#define HAVE_OPEN_MEMSTREAM 1
+
+/* Define to 1 if you have the <procinfo.h> header file. */
+/* #undef HAVE_PROCINFO_H */
+
+/* Define to 1 if you have program_invocation_short_name */
+#define HAVE_PROGRAM_INVOCATION_SHORT_NAME 1
+
+/* Define to 1 if you have the 'pstat_getproc' function. */
+/* #undef HAVE_PSTAT_GETPROC */
+
+/* Define to 1 if you have the <pwd.h> header file. */
+#define HAVE_PWD_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio_ext.h> header file. */
+#define HAVE_STDIO_EXT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the 'sysconf' function. */
+#define HAVE_SYSCONF 1
+
+/* Define to 1 if you have the <sys/dir.h> header file. */
+#define HAVE_SYS_DIR_H 1
+
+/* Define to 1 if you have the <sys/ndir.h> header file. */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if typeof works with your compiler. */
+#define HAVE_TYPEOF 1
+
+/* Define to 1 if you have the 'uid_from_user' function. */
+/* #undef HAVE_UID_FROM_USER */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the 'user_from_uid' function. */
+/* #undef HAVE_USER_FROM_UID */
+
+/* Define to 1 if you have the 'vasprintf' function. */
+#define HAVE_VASPRINTF 1
+
+/* Define to 1 if you have the <wchar.h> header file. */
+#define HAVE_WCHAR_H 1
+
+/* Define to 1 if you have the '__fpurge' function. */
+#define HAVE___FPURGE 1
+
+/* Define to 1 if you have __progname */
+#define HAVE___PROGNAME 1
+
+/* Define to 1 if you have __register_atfork */
+#define HAVE___REGISTER_ATFORK 1
+
+/* Provide ABI for setmode()/getmode() */
+#define LIBBSD_ABI_ACCMODE 1
+
+/* Provide ABI for arc4random() API */
+#define LIBBSD_ABI_ARC4RANDOM 1
+
+/* Provide ABI for arc4random_stir()/arc4random_addrandom() */
+#define LIBBSD_ABI_ARC4RANDOM_STIR 1
+
+/* Provide ABI for vasprintf()/asprintf() */
+#define LIBBSD_ABI_ASPRINTF 0
+
+/* Provide ABI for BSD getopt() */
+#define LIBBSD_ABI_BSD_GETOPT 1
+
+/* Provide ABI for closefrom() */
+#define LIBBSD_ABI_CLOSEFROM 1
+
+/* Provide ABI for err API */
+#define LIBBSD_ABI_ERR 0
+
+/* Provide ABI for errc API */
+#define LIBBSD_ABI_ERRC 1
+
+/* Provide ABI for err.h header */
+#define LIBBSD_ABI_ERR_H 1
+
+/* Provide ABI for expand_number() */
+#define LIBBSD_ABI_EXPAND_NUMBER 1
+
+/* Provide ABI for explicit_bzero() */
+#define LIBBSD_ABI_EXPLICIT_BZERO 1
+
+/* Provide ABI for fgetln/fgetwln() */
+#define LIBBSD_ABI_FGETLN 1
+
+/* Provide ABI for flopen()/flopenat() */
+#define LIBBSD_ABI_FLOPEN 1
+
+/* Provide ABI for fmtcheck() */
+#define LIBBSD_ABI_FMTCHECK 1
+
+/* Provide ABI for fpurge() */
+#define LIBBSD_ABI_FPURGE 1
+
+/* Provide ABI for freezero() */
+#define LIBBSD_ABI_FREEZERO 1
+
+/* Provide ABI for funopen() */
+#define LIBBSD_ABI_FUNOPEN 1
+
+/* Provide ABI for getbsize() */
+#define LIBBSD_ABI_GETBSIZE 1
+
+/* Provide ABI for getpeereid() */
+#define LIBBSD_ABI_GETPEEREID 1
+
+/* Provide ABI for humanize_number()/dehumanize_number() */
+#define LIBBSD_ABI_HUMANIZE_NUMBER 1
+
+/* Provide ABI for uid_from_user()/gid_from_user() */
+#define LIBBSD_ABI_ID_FROM_NAME 1
+
+/* Provide ABI for inet_net_pton() */
+#define LIBBSD_ABI_INET_NET_PTON 1
+
+/* Provide ABI for MD5 digest functions */
+#define LIBBSD_ABI_MD5 1
+
+/* Provide ABI for user_from_uid()/group_from_gid() */
+#define LIBBSD_ABI_NAME_FROM_ID 1
+
+/* Provide ABI for nlist() */
+#define LIBBSD_ABI_NLIST 1
+
+/* Provide ABI for pidfile API */
+#define LIBBSD_ABI_PIDFILE 1
+
+/* Provide ABI for setproctitle() API */
+#define LIBBSD_ABI_PROCTITLE 1
+
+/* Provide ABI for setprogname()/getprogname() */
+#define LIBBSD_ABI_PROGNAME 1
+
+/* Provide ABI for password cache API */
+#define LIBBSD_ABI_PWCACHE 1
+
+/* Provide ABI for readpassphrase() */
+#define LIBBSD_ABI_READPASSPHRASE 1
+
+/* Provide ABI for reallocarray() */
+#define LIBBSD_ABI_REALLOCARRAY 1
+
+/* Provide ABI for reallocf() */
+#define LIBBSD_ABI_REALLOCF 1
+
+/* Provide ABI for recallocarray() */
+#define LIBBSD_ABI_RECALLOCARRAY 1
+
+/* Provide ABI for BSD sort functions */
+#define LIBBSD_ABI_SORT 1
+
+/* Provide ABI for stringlist sl_*() API */
+#define LIBBSD_ABI_STRINGLIST 1
+
+/* Provide ABI for strlcpy()/strlcat() */
+#define LIBBSD_ABI_STRL 1
+
+/* Provide ABI for strmode() */
+#define LIBBSD_ABI_STRMODE 1
+
+/* Provide ABI for strnstr() */
+#define LIBBSD_ABI_STRNSTR 1
+
+/* Provide ABI for strtonum() */
+#define LIBBSD_ABI_STRTONUM 1
+
+/* Provide ABI for strtoi()/strtou() */
+#define LIBBSD_ABI_STRTOX 1
+
+/* Provide ABI for explicit time64 time_t support */
+#define LIBBSD_ABI_TIME64 0
+
+/* Provide ABI for timeconv.h API */
+#define LIBBSD_ABI_TIMECONV 1
+
+/* Provide ABI for transparent libmd support */
+#define LIBBSD_ABI_TRANSPARENT_LIBMD 1
+
+/* Provide ABI for vis API */
+#define LIBBSD_ABI_VIS 1
+
+/* Provide ABI for wcslcpy()/wcslcat() */
+#define LIBBSD_ABI_WCSL 1
+
+/* Provide API for time struct conversion macros */
+#define LIBBSD_API_TIME_MACROS 0
+
+/* Enable if the system supports 64-bit time_t */
+#define LIBBSD_SYS_HAS_TIME64 1
+
+/* The number of bits for the default system time_t ABI */
+#define LIBBSD_SYS_TIME_BITS 64
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libbsd"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "libbsd@lists.freedesktop.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libbsd"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libbsd 0.12.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libbsd"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.12.2"
+
+/* The size of 'off_t', as computed by sizeof. */
+#define SIZEOF_OFF_T 8
+
+/* The size of 'time_t', as computed by sizeof. */
+#define SIZEOF_TIME_T 8
+
+/* Define to 1 if all of the C89 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#define STDC_HEADERS 1
+
+/* Enable extensions on AIX, Interix, z/OS.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable general extensions on macOS.  */
+#ifndef _DARWIN_C_SOURCE
+# define _DARWIN_C_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable X/Open compliant socket functions that do not require linking
+   with -lxnet on HP-UX 11.11.  */
+#ifndef _HPUX_ALT_XOPEN_SOCKET_API
+# define _HPUX_ALT_XOPEN_SOCKET_API 1
+#endif
+/* Identify the host operating system as Minix.
+   This macro does not affect the system headers' behavior.
+   A future release of Autoconf may stop defining this macro.  */
+#ifndef _MINIX
+/* # undef _MINIX */
+#endif
+/* Enable general extensions on NetBSD.
+   Enable NetBSD compatibility extensions on Minix.  */
+#ifndef _NETBSD_SOURCE
+# define _NETBSD_SOURCE 1
+#endif
+/* Enable OpenBSD compatibility extensions on NetBSD.
+   Oddly enough, this does nothing on OpenBSD.  */
+#ifndef _OPENBSD_SOURCE
+# define _OPENBSD_SOURCE 1
+#endif
+/* Define to 1 if needed for POSIX-compatible behavior.  */
+#ifndef _POSIX_SOURCE
+/* # undef _POSIX_SOURCE */
+#endif
+/* Define to 2 if needed for POSIX-compatible behavior.  */
+#ifndef _POSIX_1_SOURCE
+/* # undef _POSIX_1_SOURCE */
+#endif
+/* Enable POSIX-compatible threading on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions specified by ISO/IEC TS 18661-5:2014.  */
+#ifndef __STDC_WANT_IEC_60559_ATTRIBS_EXT__
+# define __STDC_WANT_IEC_60559_ATTRIBS_EXT__ 1
+#endif
+/* Enable extensions specified by ISO/IEC TS 18661-1:2014.  */
+#ifndef __STDC_WANT_IEC_60559_BFP_EXT__
+# define __STDC_WANT_IEC_60559_BFP_EXT__ 1
+#endif
+/* Enable extensions specified by ISO/IEC TS 18661-2:2015.  */
+#ifndef __STDC_WANT_IEC_60559_DFP_EXT__
+# define __STDC_WANT_IEC_60559_DFP_EXT__ 1
+#endif
+/* Enable extensions specified by C23 Annex F.  */
+#ifndef __STDC_WANT_IEC_60559_EXT__
+# define __STDC_WANT_IEC_60559_EXT__ 1
+#endif
+/* Enable extensions specified by ISO/IEC TS 18661-4:2015.  */
+#ifndef __STDC_WANT_IEC_60559_FUNCS_EXT__
+# define __STDC_WANT_IEC_60559_FUNCS_EXT__ 1
+#endif
+/* Enable extensions specified by C23 Annex H and ISO/IEC TS 18661-3:2015.  */
+#ifndef __STDC_WANT_IEC_60559_TYPES_EXT__
+# define __STDC_WANT_IEC_60559_TYPES_EXT__ 1
+#endif
+/* Enable extensions specified by ISO/IEC TR 24731-2:2010.  */
+#ifndef __STDC_WANT_LIB_EXT2__
+# define __STDC_WANT_LIB_EXT2__ 1
+#endif
+/* Enable extensions specified by ISO/IEC 24747:2009.  */
+#ifndef __STDC_WANT_MATH_SPEC_FUNCS__
+# define __STDC_WANT_MATH_SPEC_FUNCS__ 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable X/Open extensions.  Define to 500 only if necessary
+   to make mbstate_t available.  */
+#ifndef _XOPEN_SOURCE
+/* # undef _XOPEN_SOURCE */
+#endif
+
+
+/* Version number of package */
+#define VERSION "0.12.2"
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define to 1 on platforms where this makes off_t a 64-bit type. */
+/* #undef _LARGE_FILES */
+
+/* Enable 64-bit time_t support */
+/* #undef _TIME_BITS */
+
+/* Define to 1 on platforms where this makes time_t a 64-bit type. */
+/* #undef __MINGW_USE_VC2005_COMPAT */
+
+/* Define as 'int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to '__inline__' or '__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to the type of a signed integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int64_t */
+
+/* Define to 'int' if <sys/types.h> does not define. */
+/* #undef mode_t */
+
+/* Define to 'long int' if <sys/types.h> does not define. */
+/* #undef off_t */
+
+/* Define as a signed integer type capable of holding a process identifier. */
+/* #undef pid_t */
+
+/* Define as 'unsigned int' if <stddef.h> doesn't define. */
+/* #undef size_t */
+
+/* Define as 'int' if <sys/types.h> doesn't define. */
+/* #undef ssize_t */
+
+/* Define to __typeof__ if your compiler spells it that way. */
+/* #undef typeof */
+
+/* Define as 'int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
